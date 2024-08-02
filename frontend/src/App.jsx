c2b2662be7a26c854ea180562120d09c0e211655
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-08-03 00:35:13 +0530

    started the project on 3-8-2024

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import SimpleRegistrationForm from "./components/Signup"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="flex justify-center my-8">
+    <SimpleRegistrationForm/>
+
+    </div>
+  
+    
+    </>
+  )
+}
+
+export default App
