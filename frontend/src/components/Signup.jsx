c272b9fb1b55188d10b139fdf6b719c483057cdb
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-08-03 01:17:33 +0530

    setup of backend files completed

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -42,13 +42,13 @@ import { useState } from "react";
 
     return (
        <Card color="transparent" shadow={false}>
-        <Typography variant="h4" color="blue-gray">
+        <Typography variant="h4" color="blue-gray" className="text-center">
           Sign Up
         </Typography>
-        <Typography color="gray" className="mt-1 font-normal">
+        {/* <Typography color="gray" className="mt-1 font-normal">
           Nice to meet you! Enter your details to register.
-        </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
+        </Typography> */}
+        <form className="mt-4 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="h6" color="blue-gray" className="-mb-3">
               Your Name
@@ -56,7 +56,7 @@ import { useState } from "react";
             <Input
               size="lg"
               name="username"
-              placeholder="name@mail.com"
+              placeholder="name"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
